ec33e4a6feceb2dfe067473c46f71ba7e71f3495
volkov <volkov.ivan08.12@gmail.com>
2022-05-13 12:37:12 +0300

    152192 As a customer, I want to see a welcome\home page

---
+++ shop/src/main/java/aim/traineeship/electronics/shop/entities/CartEntry.java
@@ -0,0 +1,59 @@
+package aim.traineeship.electronics.shop.entities;
+
+public class CartEntry extends Entity
+{
+	private Product product;
+	private Integer quantity;
+	private Integer entryNumber;
+	private Double totalPrice;
+
+	public CartEntry(final Product product, final Integer quantity, final Integer entryNumber,
+			final Double totalPrice)
+	{
+		this.product = product;
+		this.quantity = quantity;
+		this.entryNumber = entryNumber;
+		this.totalPrice = totalPrice;
+	}
+
+	public Product getProduct()
+	{
+		return product;
+	}
+
+	public void setProduct(final Product product)
+	{
+		this.product = product;
+	}
+
+	public Integer getQuantity()
+	{
+		return quantity;
+	}
+
+	public void setQuantity(final Integer quantity)
+	{
+		this.quantity = quantity;
+	}
+
+	public Integer getEntryNumber()
+	{
+		return entryNumber;
+	}
+
+	public void setEntryNumber(final Integer entryNumber)
+	{
+		this.entryNumber = entryNumber;
+	}
+
+	public Double getTotalPrice()
+	{
+		return totalPrice;
+	}
+
+	public void setTotalPrice(final Double totalPrice)
+	{
+		this.totalPrice = totalPrice;
+	}
+}
+
