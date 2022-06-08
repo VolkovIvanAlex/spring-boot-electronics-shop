a8c52f9997e03ea16904183e033d12d8248358f2
volkov <volkov.ivan08.12@gmail.com>
2022-06-08 17:41:59 +0300

    152196 As a customer, I want to see product details(PDP - product detail page)
    Add exception handlers and ExceprionHandlerController for mapping errors' urls and views.

--- shop/src/main/java/aim/traineeship/electronics/shop/controllers/ProductController.java
+++ shop/src/main/java/aim/traineeship/electronics/shop/controllers/ProductController.java
@@ -3,19 +3,17 @@ package aim.traineeship.electronics.shop.controllers;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import aim.traineeship.electronics.shop.dto.ProductDTO;
-import aim.traineeship.electronics.shop.exception.ItemNotFoundException;
 import aim.traineeship.electronics.shop.service.ProductService;
 
 
@@ -24,11 +22,7 @@ public class ProductController
 {
 	@Autowired
 	private ProductService productService;
-
-	private static final String FIND_PRODUCT_ERROR = "Error while trying to get product with code {}";
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
-
+	
 	@RequestMapping(value = "/products/{categoryCode}", method = RequestMethod.GET)
 	public String products(final Model model, @PathVariable("categoryCode") final String categoryCode)
 	{
@@ -40,28 +34,14 @@ public class ProductController
 	@RequestMapping(value = "/product/{productCode}", method = RequestMethod.GET)
 	public String productDetails(@PathVariable("productCode") final String productCode, final Model model)
 	{
-		try
-		{
-			final ProductDTO product = productService.getProductByCode(productCode);
-			model.addAttribute("product", product);
-		}
-		catch (final NoSuchElementException noSuchElementException)
-		{
-			LOGGER.error(FIND_PRODUCT_ERROR, productCode, noSuchElementException);
-			throw new ItemNotFoundException();
-		}
+		final ProductDTO product = productService.getProductByCode(productCode);
+		model.addAttribute("product", product);
 		return "pdp";
 	}
 
-	@ExceptionHandler(ItemNotFoundException.class)
-	public String notFoundRedirect()
+	@ExceptionHandler(NoSuchElementException.class)
+	public String noElement(final HttpServletRequest request)
 	{
-		return "redirect:/Error-404";
-	}
-
-	@GetMapping("/Error-404")
-	public String notFoundShow()
-	{
-		return "error/404";
+		return "redirect:/404";
 	}
 }
